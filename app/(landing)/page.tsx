3f3db82d5812adfdf43c5edc1758cc9a433ba19c
imrulkayessifat <kayesimrul224@gmail.com>
2023-11-04 01:19:07 +0600

    work with image slider

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,3 +1,8 @@
+// "use client";
+
+import { useCallback } from "react";
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
 
 import Navbar from '@/components/navbar'
 import { Social } from '@/components/social'
@@ -8,6 +13,14 @@ import Tools from '@/components/tools'
 import ImageSlider from '@/components/imageslider'
 
 export default function Landing() {
+  const particlesInit = useCallback(async engine => {
+    console.log(engine);
+    await loadFull(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(async container => {
+    await console.log(container);
+  }, []);
   return (
     <>
       {/* navbar */}
@@ -24,7 +37,75 @@ export default function Landing() {
 
       <Social />
       <Info />
-
+      {/* <Particles
+        id="tsparticles"
+        init={particlesInit}
+        loaded={particlesLoaded}
+        options={{
+          fpsLimit: 30,
+          interactivity: {
+            events: {
+              onClick: {
+                enable: true,
+                mode: "push",
+              },
+              onHover: {
+                enable: true,
+                mode: "repulse",
+              },
+              resize: true,
+            },
+            modes: {
+              push: {
+                quantity: 4,
+              },
+              repulse: {
+                distance: 200,
+                duration: 0.4,
+              },
+            },
+          },
+          particles: {
+            // color: {
+            //   value: "#0d47a1",
+            // },
+            links: {
+              color: "#0d47a1",
+              distance: 150,
+              enable: true,
+              opacity: 0.5,
+              width: 1,
+            },
+            move: {
+              direction: "none",
+              enable: true,
+              outModes: {
+                default: "bounce",
+              },
+              random: false,
+              speed: 6,
+              straight: false,
+            },
+            number: {
+              density: {
+                enable: true,
+                area: 800,
+              },
+              value: 80,
+            },
+            opacity: {
+              value: 0.5,
+            },
+            shape: {
+              type: "circle",
+            },
+            size: {
+              value: { min: 1, max: 5 },
+            },
+          },
+          detectRetina: true,
+        }}
+      /> */}
     </>
   )
 }
