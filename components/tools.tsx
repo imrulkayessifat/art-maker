bdf27ae7f3175ab5cd4a34b21d4d2902ba3f6a2b
imrulkayessifat <kayesimrul224@gmail.com>
2023-11-16 15:01:08 +0600

    tools section Design fix

--- components/tools.tsx
+++ components/tools.tsx
@@ -1,15 +1,17 @@
+import Image from "next/image"
+
 import { Button } from "@/components/ui/button"
 import {
     Card,
     CardContent,
     CardFooter,
 } from "@/components/ui/card"
+import { tools } from "@/lib/tools"
 
-import Image from "next/image"
 
 const Tools = () => {
     return (
-        <div className='flex flex-col gap-6 m-8'>
+        <div className='flex flex-col w-full gap-6 m-8'>
             <div className='flex flex-col gap-1'>
                 <p className='font-sans text-base'>
                     DISCOVER THE POTENTIAL OF
@@ -24,128 +26,35 @@ const Tools = () => {
                     inspiration and take your artworks to new heights with our AI image generator tools.
                 </p>
             </div>
-            <div className='grid grid-cols-1 md:grid-cols-2 gap-10'>
-                <Card className="pb-2">
-                    <CardContent className="p-2">
-                        <Image
-                            src={"https://cdn1.imagine.art/imagine-frontend/assets/images/TexttoImage.webp"}
-                            alt={""}
-                            layout='responsive'
-                            width={500}
-                            height={281}
-                            objectFit='cover'
-                            className="rounded-md"
-                        />
-                    </CardContent>
-                    <CardFooter className="flex rounded-md bg-gray-800 mx-2 justify-between">
-                        <div>
-                            <p className="font-sans text-white text-base md:text-xl">
-                                Text to Image
-                            </p>
-                            <p className="font-sans text-white text-sm md:text-base">
-                                Transform your words into stunning AI visuals with our text-to-image feature
-                            </p>
-                        </div>
-                        <Button variant={"sky"}>Generate</Button>
-                    </CardFooter>
-                </Card>
-
-                <Card className="pb-2">
-                    <CardContent className="p-2">
-                        <Image
-                            src={"https://cdn1.imagine.art/imagine-frontend/assets/images/ImageRemix.webp"} alt={""}
-                            layout='responsive'
-                            width={500}
-                            height={281}
-                            objectFit='cover'
-                            className="rounded-md"
-                        />
-                    </CardContent>
-                    <CardFooter className="flex rounded-md bg-gray-800 mx-2 justify-between">
-                        <div>
-                            <p className="font-sans text-white text-base md:text-xl">
-                                Image Remix
-                            </p>
-                            <p className="font-sans text-white text-sm md:text-base">
-                                Add creativity to your photos and turn them into beautiful artworks.
-                            </p>
-                        </div>
-                        <Button variant={"sky"}>Generate</Button>
-                    </CardFooter>
-                </Card>
-
-                <Card className="pb-2">
-                    <CardContent className="p-2">
-                        <Image
-                            src={"https://cdn1.imagine.art/imagine-frontend/assets/images/ImageRemix.webp"} alt={""}
-                            layout='responsive'
-                            width={500}
-                            height={281}
-                            objectFit='cover'
-                            className="rounded-md"
-                        />
-                    </CardContent>
-                    <CardFooter className="flex rounded-md bg-gray-800 mx-2 justify-between">
-                        <div>
-                            <p className="font-sans text-white text-base md:text-xl">
-                                Inpainting
-                            </p>
-                            <p className="font-sans text-white text-sm md:text-base">
-                                Say goodbye to unwanted objects with our AI Inpainting feature!
-                            </p>
-                        </div>
-                        <Button variant={"sky"}>Generate</Button>
-                    </CardFooter>
-                </Card>
-
-                <Card className="pb-2">
-                    <CardContent className="p-2">
-                        <Image
-                            src="https://cdn1.imagine.art/imagine-frontend/assets/images/ExpandImage.webp" alt={""}
-                            layout='responsive'
-                            width={500}
-                            height={281}
-                            objectFit='cover'
-                            className="rounded-md"
-                        />
-                    </CardContent>
-                    <CardFooter className="flex rounded-md bg-gray-800 mx-2 justify-between">
-                        <div>
-                            <p className="font-sans text-white text-base md:text-xl">
-                                Expand Image
-                            </p>
-                            <p className="font-sans text-white text-sm md:text-base">
-                                Our AI Expand Image function will help you broaden your horizons.
-                            </p>
-                        </div>
-                        <Button variant={"sky"}>Generate</Button>
-                    </CardFooter>
-                </Card>
-
-                <Card className="pb-2">
-                    <CardContent className="p-2">
-                        <Image
-                            src="https://cdn1.imagine.art/imagine-frontend/assets/images/BackgroundReplace.webp" alt={""}
-                            layout='responsive'
-                            width={500}
-                            height={281}
-                            objectFit='cover'
-                            className="rounded-md"
-                        />
-                    </CardContent>
-                    <CardFooter className="flex rounded-md bg-gray-800 mx-2 justify-between">
-                        <div>
-                            <p className="font-sans text-white text-base md:text-xl">
-                                Background Replace
-                            </p>
-                            <p className="font-sans text-white text-sm md:text-base">
-                                Transform your photos with our AI Background Replace feature to look charm!
-                            </p>
-                        </div>
-                        <Button variant={"sky"}>Generate</Button>
-                    </CardFooter>
-                </Card>
-
+            <div className='grid grid-cols-1 md:grid-cols-2 items-center justify-center w-full gap-5'>
+                {
+                    tools.map((tool, i) => (
+                        <Card className="pb-2 max-h-[440px] max-w-[458px]">
+                            <CardContent className="p-2">
+                                <Image
+                                    src={tool.img}
+                                    alt={""}
+                                    layout='responsive'
+                                    width={240}
+                                    height={281}
+                                    objectFit='cover'
+                                    className="rounded-md"
+                                />
+                            </CardContent>
+                            <CardFooter className="flex pt-3 rounded-md bg-gray-800 mx-2 justify-between">
+                                <div>
+                                    <p className="font-sans text-white text-base md:text-xl">
+                                        {tool.p1}
+                                    </p>
+                                    <p className="font-sans text-white text-sm md:text-base">
+                                        {tool.p2}
+                                    </p>
+                                </div>
+                                <Button variant={"sky"}>Generate</Button>
+                            </CardFooter>
+                        </Card>
+                    ))
+                }
 
             </div>
         </div>
---
+++ lib/tools.ts
@@ -0,0 +1,27 @@
+export const tools =  [
+    {
+        img : 'https://cdn1.imagine.art/imagine-frontend/assets/images/TexttoImage.webp',
+        p1:'Text to Image',
+        p2:'Transform your words into stunning AI visuals with our text-to-image feature,'
+    },
+    {
+        img: 'https://cdn1.imagine.art/imagine-frontend/assets/images/ImageRemix.webp',
+        p1:'Image Remix',
+        p2:'Add creativity to your photos and turn them into beautiful artworks.'
+    },
+    {
+        img: 'https://cdn1.imagine.art/imagine-frontend/assets/images/Inpainting.webp',
+        p1:'Inpainting',
+        p2:'Say goodbye to unwanted objects with our AI Inpainting feature!'
+    },
+    {
+        img:'https://cdn1.imagine.art/imagine-frontend/assets/images/ExpandImage.webp',
+        p1:'Expand Image',
+        p2:' Our AI Expand Image function will help you broaden your horizons.'
+    },
+    {
+        img:'https://cdn1.imagine.art/imagine-frontend/assets/images/BackgroundReplace.webp',
+        p1:'Background Replace',
+        p2:'Transform your photos with our AI Background Replace feature to look charm!'
+    }
+]
