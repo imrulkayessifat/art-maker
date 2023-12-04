e7f161719d7dc3bd78d90e52b19e5fd878a255ef
imrulkayessifat <kayesimrul224@gmail.com>
2023-12-04 12:42:46 +0600

    fix image slider bug

--- components/imageslider.tsx
+++ components/imageslider.tsx
@@ -1,80 +1,68 @@
 "use client";
 
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef, useCallback } from 'react';
 import Image from 'next/image';
+import { useInView } from 'react-intersection-observer';
+
+import { images } from '@/lib/slider/images';
+import { secondImages } from '@/lib/slider/secondImages';
 
 const ImageSlider = () => {
-    const images = [
-        'https://cdn1.imagine.art/imagine-frontend/assets/images/RowImageSlider1.webp',
-        "https://cdn1.imagine.art/imagine-frontend/assets/images/RowImageSlider32.webp",
-        "https://cdn1.imagine.art/imagine-frontend/assets/images/RowImageSlider26.webp",
-        'https://cdn1.imagine.art/imagine-frontend/assets/images/RowImageSlider34.webp',
-        "https://cdn1.imagine.art/imagine-frontend/assets/images/RowImageSlider18.webp",
-        "https://cdn1.imagine.art/imagine-frontend/assets/images/RowImageSlider10.webp",
-        'https://cdn1.imagine.art/imagine-frontend/assets/images/RowImageSlider27.webp',
-        "https://cdn1.imagine.art/imagine-frontend/assets/images/RowImageSlider4.webp",
-        "https://cdn1.imagine.art/imagine-frontend/assets/images/RowImageSlider16.webp",
-        'https://cdn1.imagine.art/imagine-frontend/assets/images/RowImageSlider5.webp',
-        "https://cdn1.imagine.art/imagine-frontend/assets/images/RowImageSlider35.webp",
-        "https://cdn1.imagine.art/imagine-frontend/assets/images/RowImageSlider8.webp",
-        "https://cdn1.imagine.art/imagine-frontend/assets/images/RowImageSlider11.webp",
-        'https://cdn1.imagine.art/imagine-frontend/assets/images/RowImageSlider31.webp',
-        "https://cdn1.imagine.art/imagine-frontend/assets/images/RowImageSlider36.webp",
-        "https://cdn1.imagine.art/imagine-frontend/assets/images/RowImageSlider20.webp",
-        "https://cdn1.imagine.art/imagine-frontend/assets/images/RowImageSlider23.webp",
-        'https://cdn1.imagine.art/imagine-frontend/assets/images/RowImageSlider17.webp',
-        "https://cdn1.imagine.art/imagine-frontend/assets/images/RowImageSlider30.webp",
-        "https://cdn1.imagine.art/imagine-frontend/assets/images/RowImageSlider40.webp",
-    ];
 
-    const secondImages = [
-        'https://cdn1.imagine.art/imagine-frontend/assets/images/RowImageSlider22.webp',
-        "https://cdn1.imagine.art/imagine-frontend/assets/images/RowImageSlider21.webp",
-        "https://cdn1.imagine.art/imagine-frontend/assets/images/RowImageSlider13.webp",
-        'https://cdn1.imagine.art/imagine-frontend/assets/images/RowImageSlider28.webp',
-        "https://cdn1.imagine.art/imagine-frontend/assets/images/RowImageSlider41.webp",
-        "https://cdn1.imagine.art/imagine-frontend/assets/images/RowImageSlider24.webp",
-        'https://cdn1.imagine.art/imagine-frontend/assets/images/RowImageSlider14.webp',
-        "https://cdn1.imagine.art/imagine-frontend/assets/images/RowImageSlider3.webp",
-        "https://cdn1.imagine.art/imagine-frontend/assets/images/RowImageSlider12.webp",
-        'https://cdn1.imagine.art/imagine-frontend/assets/images/RowImageSlider29.webp',
-        "https://cdn1.imagine.art/imagine-frontend/assets/images/RowImageSlider15.webp",
-        "https://cdn1.imagine.art/imagine-frontend/assets/images/RowImageSlider25.webp",
-        "https://cdn1.imagine.art/imagine-frontend/assets/images/RowImageSlider39.webp",
-        'https://cdn1.imagine.art/imagine-frontend/assets/images/RowImageSlider2.webp',
-        "https://cdn1.imagine.art/imagine-frontend/assets/images/RowImageSlider6.webp",
-        "https://cdn1.imagine.art/imagine-frontend/assets/images/RowImageSlider7.webp",
-        "https://cdn1.imagine.art/imagine-frontend/assets/images/RowImageSlider33.webp",
-        'https://cdn1.imagine.art/imagine-frontend/assets/images/RowImageSlider38.webp',
-        "https://cdn1.imagine.art/imagine-frontend/assets/images/RowImageSlider37.webp",
-        "https://cdn1.imagine.art/imagine-frontend/assets/images/RowImageSlider19.webp",
-    ];
+    const [scrollPosition1, setScrollPosition1] = useState(-500);
+    const [scrollPosition2, setScrollPosition2] = useState(-500);
+    const [inViewRef1, inView1] = useInView({
+        threshold: 0.2,
+    });
+    const [inViewRef2, inView2] = useInView({
+        threshold: 0.2,
+    });
 
-    const [scrollPosition, setScrollPosition] = useState(0);
-    
-    const handleScroll = () => {
-        if (window.scrollY - 3400 < -200) {
-            setScrollPosition(window.scrollY - 3400);
-        } else {
-            setScrollPosition(window.scrollY - 4300);
+    const [prevScrollValue1, setPrevScrollValue1] = useState(window.scrollY);
+    const [prevScrollValue2, setPrevScrollValue2] = useState(window.scrollY);
+
+    const handleScroll1 = useCallback(() => {
+        const currentScrollPos = window.scrollY;
+        if (inView1) {
+            if (currentScrollPos > prevScrollValue1) {
+                setScrollPosition1((prev) => prev + 10)
+            } else {
+                setScrollPosition1((prev) => prev - 10)
+            }
         }
-    };
+        setPrevScrollValue1(currentScrollPos);
+    }, [window.scrollY, prevScrollValue1, inView1])
+
+
+    const handleScroll2 = useCallback(() => {
+        const currentScrollPos = window.scrollY;
+        if (inView2) {
+            if (currentScrollPos > prevScrollValue2) {
+                setScrollPosition2((prev) => prev - 10)
+            } else {
+                setScrollPosition2((prev) => prev + 10)
+            }
+        }
+        setPrevScrollValue2(currentScrollPos);
+    }, [window.scrollY, prevScrollValue2, inView2])
 
     useEffect(() => {
-        window.addEventListener('scroll', handleScroll);
+        window.addEventListener('scroll', handleScroll1);
+        window.addEventListener('scroll', handleScroll2);
         return () => {
-            window.removeEventListener('scroll', handleScroll);
+            window.removeEventListener('scroll', handleScroll1);
+            window.removeEventListener('scroll', handleScroll2);
         };
-    }, []);
+    }, [prevScrollValue1, prevScrollValue2, inView1, inView2]);
 
-    const sliderStyle: React.CSSProperties = {
-        transform: `translate3d(${scrollPosition + 200}px,0px,0px)`,
+    const sliderStyle1: React.CSSProperties = {
+        transform: `translate3d(${scrollPosition1}px,0px,0px)`,
         transition: 'transform 0.5s ease-in-out',
         position: 'relative',
     };
 
-    const secondSliderStyle: React.CSSProperties = {
-        transform: `translate3d(${-scrollPosition - 1200}px,0px,0px)`,
+    const sliderStyle2: React.CSSProperties = {
+        transform: `translate3d(${scrollPosition2}px,0px,0px)`,
         transition: 'transform 0.5s ease-in-out',
         position: 'relative',
     };
@@ -91,33 +79,37 @@ const ImageSlider = () => {
                     {"See Imagine's potential unfold and create captivating art on our Text-to-Art Generator"}
                 </p>
             </div>
-            
+
             <div className='flex flex-col gap-2 overflow-hidden'>
-                <div style={sliderStyle} className='flex gap-2 items-center justify-between'>
-                    {images.map((image, i) => (
-                        <Image
-                            key={i}
-                            src={image}
-                            alt={''}
-                            width={"250"}
-                            height={"250"}
-                            className='rounded-md'
-                        />
-                    ))}
+                <div ref={inViewRef1} className='intersection-observer-target'>
+                    <div style={sliderStyle1} className='flex gap-2 items-center justify-between'>
+                        {images.map((image, i) => (
+                            <Image
+                                key={i}
+                                src={image}
+                                alt={''}
+                                width={"250"}
+                                height={"250"}
+                                className='rounded-md'
+                            />
+                        ))}
+                    </div>
                 </div>
-                {/* secondimageslide */}
-                <div style={secondSliderStyle} className='flex gap-2 items-center justify-between'>
-                    {secondImages.map((image, i) => (
-                        <Image
-                            key={i}
-                            src={image}
-                            alt={''}
-                            width={"250"}
-                            height={"250"}
-                            className='rounded-md'
-                        />
-                    ))}
+                <div ref={inViewRef2} className='intersection-observer-target'>
+                    <div style={sliderStyle2} className='flex gap-2 items-center justify-between'>
+                        {secondImages.map((image, i) => (
+                            <Image
+                                key={i}
+                                src={image}
+                                alt={''}
+                                width={"250"}
+                                height={"250"}
+                                className='rounded-md'
+                            />
+                        ))}
+                    </div>
                 </div>
+
             </div>
         </div>
     )
